880acb0aafd221d77add8c1c943e75f5e49d5b20
slmf <wuhaihui@outlook.com>
2019-12-05 14:11:42 +0800

    master

--- cprimer/chapter10.2.c
+++ cprimer/chapter10.2.c
@@ -2,7 +2,7 @@
 //指针操作
 int main()
 {
-    int urn[5] = {100, 200, 300, 400, 500}; //程序清单10.13
+    /* int urn[5] = {100, 200, 300, 400, 500}; //程序清单10.13
     int *ptr1, *ptr2, *ptr3;
     ptr1 = urn;
     ptr2 = &urn[2];
@@ -22,17 +22,21 @@ int main()
     printf("\nvalues after ptr2--:\n");
     printf("ptr2=%p,*ptr2=%d,&ptr2=%p\n", ptr2, *ptr2, &ptr2);
     --ptr1;
-    ++ptr2;
+    ++ptr2; */
 
     int zippo[4][2] = {{2, 4}, {6, 8}, {1, 3}, {5, 7}};
-    printf("zippo=%p,   zippo+1=%p\n", zippo, zippo + 1);
+    /* printf("zippo=%p,   zippo+1=%p\n", zippo, zippo + 1);
     printf("zippo[0]=%p, zippo[0]+1=%p\n", zippo[0], zippo[0] + 1);
     printf("*zippo=%p,   *zippo+1=%p\n", *zippo, *zippo + 1);
     printf("zippo[0][0]=%d\n", zippo[0][0]);
     printf("*zippo[0]=%d\n", *zippo[0]);
     printf("**zippo=%d\n", **zippo);
     printf("  zippo[2][1]=%d\n", zippo[2][1]);
-    printf("*(*(zippo+2)+1)=%d\n", *(*(zippo + 2) + 1));
+    printf("*(*(zippo+2)+1)=%d\n", *(*(zippo + 2) + 1)); */
+
+    int(*pz)[2];
+    pz = zippo;
+    printf("  pz=%p,  pz+1=%p\n", pz, pz + 1);
 
     return 0;
 }
